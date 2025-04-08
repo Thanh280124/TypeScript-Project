fc89360b25daa1a2f98e55849bf83ad1cc24256c
Pham Thanh <thanhpham28124@gmail.com>
2025-04-08 10:59:52 +0300

    Filter-Ecom

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,11 +1,28 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
+import TopSellers from "./components/TopSellers";
+import PopularBlogs from "./components/PopularBlogs";
 
-
-const App = () => {
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="rounded w-full  flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+          <div>
+            <TopSellers />
+            <PopularBlogs />
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
